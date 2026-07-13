dc83c2e1dc9499ef7f84b550bf7b5e90e4a6c518
StepLidia <StepLidia@users.noreply.github.com>
2026-07-13 18:26:48 +0200

    add db migrations

--- solar-profitability-estimator/src/SolarProfitabilityEstimator.Infrastructure/Migrations/20260713162028_AddTrackingId.Designer.cs
+++ solar-profitability-estimator/src/SolarProfitabilityEstimator.Infrastructure/Migrations/20260713162028_AddTrackingId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SolarProfitabilityEstimator.Infrastructure;
 
@@ -11,9 +12,11 @@ using SolarProfitabilityEstimator.Infrastructure;
 namespace SolarProfitabilityEstimator.Infrastructure.Migrations
 {
     [DbContext(typeof(SolarDbContext))]
-    partial class SolarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260713162028_AddTrackingId")]
+    partial class AddTrackingId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ solar-profitability-estimator/src/SolarProfitabilityEstimator.Infrastructure/Migrations/20260713162028_AddTrackingId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolarProfitabilityEstimator.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrackingId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TrackingId",
+                table: "SolarEstimates",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrackingId",
+                table: "SolarEstimates");
+        }
+    }
+}
